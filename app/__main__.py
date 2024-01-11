b8e9ec8da56f9816fb930d87eea5f2707df57f18
HyPerMax5 <58142999+HyPerMax5@users.noreply.github.com>
2024-01-11 14:55:04 +0100

    Main passes variables now but it no longer works as intended...

--- app/__main__.py
+++ app/__main__.py
@@ -20,56 +20,59 @@ rythm_score:float = 0
 max_score:float = 100
 min_score:float = 0
 
-karaoke_performance = './app/JUDGEMENT-審判_(Vocals).wav'
-song_vocals = './app/audio.wav'
+karaoke_performance = './app/karaoke_performance.wav'
+song_vocals = './app/JUDGEMENT-審判_(Vocals).wav'
 
 
-def extract_singer_rythm(q1):
+def extract_singer_rythm(queue):
 	print("Loading performance...")
 	karaoke_performance_y, karaoke_performance_sr = librosa.load(karaoke_performance)
 	print("Extracting performance rythm...")
 	tempo, beat_frames = librosa.beat.beat_track(y=karaoke_performance_y, sr=karaoke_performance_sr)
 	singer_beat_frames = beat_frames
+	queue.put((1, singer_beat_frames))
 	singer_extraction_finished = True
 	print("Singer rythm done!");print("Singer extraction finished: ", singer_extraction_finished)
 	return singer_extraction_finished
 
-def extract_song_rythm(q2):
+def extract_song_rythm(queue):
 	print("Loading song...")
 	song_vocals_y, song_vocals_sr = librosa.load(song_vocals)
 	print("Extracting song rythm...")
 	tempo, beat_frames = librosa.beat.beat_track(y=song_vocals_y, sr=song_vocals_sr)
 	song_beat_frames = beat_frames
+	queue.put((2, song_beat_frames))
 	song_extraction_finished = True
 	print("Song rythm done!");print("Song extraction finished: ", song_extraction_finished)
 	return song_extraction_finished, song_beat_frames
 
-def score_rythm():
+def score_rythm(singer_beat_frames, song_beat_frames):
 	print("Scoring Rythm...!")
 	rhythm_deviation = np.subtract(np.mean(np.abs(singer_beat_frames)), np.mean(np.abs(song_beat_frames)))
-	rhythm_score = max_score - (rhythm_deviation / 10)
-	rhythm_score = max(min(rhythm_score, max_score), min_score)
+	rhythm_score = max_score - (rhythm_deviation / 100)
+	#rhythm_score = max(min(rhythm_score, max_score), min_score)
 	print("Rhythm Score:", rhythm_score)
 	return rhythm_score
 
 
 if __name__=='__main__':
 
-	q1 = Queue()
-	q2 = Queue()
-	p1 = Process(target = extract_singer_rythm, args=(q1,))
+	queue = Queue()
+	p1 = Process(target = extract_singer_rythm, args=(queue,))
 	p1.start()
-	p2 = Process(target = extract_song_rythm, args=(q2,))
+	p2 = Process(target = extract_song_rythm, args=(queue,))
 	p2.start()
 	p1.join()
 	p2.join()
-	value1 = q1.get()
-	value2 = q2.get()
-	print(value1)
+	result1 = queue.get()
+	result2 = queue.get()
+
+	print(result1[1])
 	print("Space")
-	print(value2)
+	print(result2[1])
+
 	while singer_extraction_finished == True and song_extraction_finished == True:
 		print("Waiting....")
 	else:
-		#score_rythm()
+		score_rythm(result1[1], result2[1])
 		print("Weeee, done!")
--- app/example_rythm.py
+++ app/example_rythm.py
@@ -4,8 +4,8 @@ import librosa
 max_score:float = 100
 min_score:float = 0
 
-audio = './app/JUDGEMENT-審判_(Vocals).wav'
-ref = './app/karaoke_performance.wav'
+audio = './app/karaoke_performance.wav'
+ref = './app/JUDGEMENT-審判_(Vocals).wav'
 
 audio_y, audio_sr = librosa.load(ref)
 ref_y, ref_sr = librosa.load(ref)
@@ -16,12 +16,12 @@ ref_tempo, ref_beat_frames = librosa.beat.beat_track(y=ref_y, sr=ref_sr)
 
 
 
-rhythm_deviation = np.subtract(np.mean(np.abs(beat_frames)), np.mean(np.abs(ref_beat_frames)))
+rhythm_deviation = np.subtract(np.mean(np.abs(ref_beat_frames)), np.mean(np.abs(beat_frames)))
 print(rhythm_deviation)
 
 #rhythm_deviation = np.mean(np.abs(beat_frames - ref_beat_frames))
 rhythm_score = max_score - (rhythm_deviation / 10)
-rhythm_score = max(min(rhythm_score, max_score), min_score)
+#rhythm_score = max(min(rhythm_score, max_score), min_score)
 print("Rhythm Score:", rhythm_score)
 
 
